7a1a246a6f91ccbf8259a138c0ceb963341de7d1
TJ Jackson <elitefourfresh@gmail.com>
2019-08-20 16:15:00 -0400

    timer functionality works

--- assets/js/JStimerapp.js
+++ assets/js/JStimerapp.js
@@ -1,8 +1,36 @@
-let timer = setInterval(timeHandler, 1000);
+// TIMER
+let timer = setInterval(timerHandler, 1000);
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
-function timeHandler() {
+function timerHandler() {
+    seconds++;
+    if(seconds == 60) {
+        seconds = 0;
+        minutes++;
+    }
+    if(minutes == 60) {
+        minutes = 0;
+        hours++;
+    }
+    
+    displayTime();
+};
 
+function displayTime() {
+    let secDouble;
+    let minDouble;
+    let hourDouble;
+    let timerElement = document.getElementById("timer");
+    if(seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    if(minutes < 10) {
+        minDouble = "0" + minutes;
+    }
+    if(hours < 10) {
+        hourDouble = "0" + hours;
+    }
+    timerElement.innerHTML = hourDouble + " : " + minDouble + " : " + seconds;
 }
